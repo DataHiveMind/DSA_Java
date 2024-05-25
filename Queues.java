97ef58a5b80424919ca097eeaefb1f03f5037140
DataHiveMind <77356196+DataHiveMind@users.noreply.github.com>
2024-05-25 06:50:35 -0400

    T1

--- Queues.java
+++ Queues.java
@@ -1,7 +1,20 @@
 import java.util.Queue;
+/*
+
+Queue = Linear Data Structure
+
+add = enqueue, offer()
+remove = dequeue, poll()
+
+*/
 
 public class Queue{
     public static void main(Strings [] arg){
+        Queue<String> queue = new LinkedList<String>();
 
+        // Insert element into this queue
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
     }
 }
